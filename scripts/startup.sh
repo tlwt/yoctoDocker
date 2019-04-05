076b85fc3674dbe333464d88768fb65858e6af33
Till Witt <mail@tillwitt.de>
2019-04-05 10:34:41 +0200

    updating env variables

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /data
 # Step 1 - run once
 git config --global user.name "$GIT_NAME" && git config --global user.email "$GIT_EMAIL" && echo "N" | repo init -u https://source.codeaurora.org/external/imx/imx-manifest  -b imx-linux-rocko -m imx-4.9.88-2.0.0_ga.xml
 
@@ -7,14 +7,14 @@ git config --global user.name "$GIT_NAME" && git config --global user.email "$GI
 repo sync
 
 # Step 3- run once (choose from chapter 5.1)
-DISTRO=$DISTRO MACHINE=$MACHINE source fsl-setup-release.sh -b build_$MACHINE
+DISTRO=$Y_DISTRO MACHINE=$Y_MACHINE source fsl-setup-release.sh -b build_$Y_MACHINE
 
 #Step 3.b - for rebuild run without DISTRO
 #MACHINE=$MACHINE source fsl-setup-release.sh -b build_$MACHINE
 
 # Step 4 check conf folders
 
-sed -i 's/INHERIT += "sanity"/INHERIT += ""/g' /data/imx-yocto-bsp/sources/poky/meta/conf/sanity.conf
+sed -i 's/INHERIT += "sanity"/INHERIT += ""/g' /data/sources/poky/meta/conf/sanity.conf
 
 #Step 5 - choose project image (5.2)
-bitbake $IMAGE
+bitbake $Y_IMAGE
