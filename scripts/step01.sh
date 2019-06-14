4f26b496290cdf9eef348d7866ecdd2fe3c6b567
Till Witt <mail@tillwitt.de>
2019-06-14 14:55:23 +0200

    example for #12 added [CI SKIP]

---
+++ scripts/step01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo inside step01.sh
+# you can update the repository here ...
+
+#cd /drone/custombuild/sources/meta-java
+#git submodule update --init --recursive
+#git checkout rocko
